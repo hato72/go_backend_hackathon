7364c2bb3214ab47237f9a6e3f3032fe7d6bd740
hato72 <hatotori72@gmail.com>
2024-04-19 19:27:07 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+https://qiita.com/tkms13/items/74a9ea4b41302323c4b1
+
+docker compose build
+
+docker compose run --rm backend sh
+
+air init
+
+docker compose up
---
+++ backend/src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// e := echo.New()
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "Hello, World!")
+	// })
+	// e.Logger.Fatal(e.Start(":8080"))
+	fmt.Println("hello world")
+}
