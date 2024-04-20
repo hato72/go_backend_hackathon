935c680bb303d871eaf49ae5c0c53cbdd3e0a012
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-20 20:06:45 +0900

    add :  テストコードを追加

--- backend/src/main.go
+++ backend/src/main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"net/http"
-
+	"database/sql"
 	"github.com/labstack/echo/v4"
+	"fmt"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+    db, err := sql.Open("postgres", "user=hato password=hato72 dbname=hato host=db sslmode=disable")
+    if err != nil {
+        fmt.Println("DB connection error:", err)
+        return
+    }
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+        fmt.Println("DB ping error:", err)
+        return
+    }
+
+    // Query current date and time from the database
+    var currentTime string
+    err = db.QueryRow("SELECT NOW()").Scan(&currentTime)
+    if err != nil {
+        fmt.Println("DB query error:", err)
+        return
+    }
+
+    fmt.Println("Current date and time:", currentTime)
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
