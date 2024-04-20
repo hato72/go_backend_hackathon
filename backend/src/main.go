e32fae080fc3c606be9364c2eab2e24867e214af
hato72 <hatotori72@gmail.com>
2024-04-20 18:50:20 +0900

    db接続できない

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,16 +1,41 @@
 package main
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/labstack/echo/v4"
+// 	"github.com/labstack/echo/v4"
+// )
+
+// func main() {
+// 	e := echo.New()
+// 	e.GET("/", func(c echo.Context) error {
+// 		return c.String(http.StatusOK, "Hello, World!")
+// 	})
+// 	e.Logger.Fatal(e.Start(":8080"))
+// 	//fmt.Println("hello world hallo")
+// }
+
+import (
+	"backend/src/controller"
+	"backend/src/db"
+	"backend/src/repository"
+	"backend/src/router"
+	"backend/src/usecase"
+	"backend/src/validator"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
-	//fmt.Println("hello world hallo")
+	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	//taskValidator := validator.NewTaskValidator()
+	userRepository := repository.NewUserRepository(db) //各コンストラクタ起動
+	//taskRepository := repository.NewTaskRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	//taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
+	userController := controller.NewUserController(userUsecase)
+	//taskController := controller.NewTaskController(taskUsecase)
+	//e := router.NewRouter(userController, taskController)
+	e := router.NewRouter(userController)
+	e.Logger.Fatal(e.Start(":8080")) //サーバー起動
+	//docker,pgAdminを起動->docker compose up -d -> bashでGO_ENV=dev go run main.go
 }
