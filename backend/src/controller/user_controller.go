f0848ea8f696e2edb258f02596ad5712df020c1e
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-25 16:07:22 +0000

    add : update処理を追加

--- backend/src/controller/user_controller.go
+++ backend/src/controller/user_controller.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
@@ -14,6 +15,7 @@ type IUserController interface {
 	SignUp(c echo.Context) error
 	Login(c echo.Context) error
 	Logout(c echo.Context) error
+	Update(c echo.Context) error
 	CsrfToken(c echo.Context) error
 }
 
@@ -71,6 +73,35 @@ func (uc *UserController) Logout(c echo.Context) error {
 	cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie) //httpレスポンスに含める
 	return c.NoContent(http.StatusOK)
+}
+
+func (uc *UserController) Update(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	authUser := c.Get("user").(*jwt.Token)
+	claims := authUser.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	user.ID = uint(userId.(float64))
+	newEmail := c.FormValue("email")
+	newName := c.FormValue("name")
+	newPassword := c.FormValue("password")
+	iconFile, err := c.FormFile("icon")
+	if err != nil {
+		if err != http.ErrMissingFile {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+	}
+
+	userRes, err := uc.uu.Update(user, newEmail, newName, newPassword, iconFile)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, userRes)
 }
 
 func (uc *UserController) CsrfToken(c echo.Context) error {
