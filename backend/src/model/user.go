a91ece8dec2ff9d5c25e1e82f189271df1c0ad97
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-25 15:56:27 +0000

    add : userのスキーマにicon_urlを追加

--- backend/src/model/user.go
+++ backend/src/model/user.go
@@ -5,10 +5,12 @@ type User struct {
 	Name     string `json:"name"`
 	Email    string `json:"email" gorm:"unique"` //重複を許さない
 	Password string `json:"password"`
+	IconUrl *string `json:"icon_url"`
 }
 
 type UserResponse struct {
 	ID    uint   `json:"id" gorm:"primaryKey"`
 	Name  string `json:"name"`
 	Email string `json:"email" gorm:"unique"`
+	IconUrl *string `json:"icon_url"`
 }
