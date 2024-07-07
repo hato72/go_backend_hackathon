32080348f2f75d95aefc8b21ef969a661a516570
hato72 <139688965+hato72@users.noreply.github.com>
2024-07-08 02:31:40 +0900

    Update user_controller.go

--- backend/controller/user_controller.go
+++ backend/controller/user_controller.go
@@ -2,6 +2,7 @@ package controller
 
 //user_usecaseで実装したユーザー登録、ログイン、ログアウト、ユーザー情報更新メソッドを呼び出す
 //このプログラムの処理が一番外側に位置し、routerでAPIとして呼び出される
+//routerで受け取ったボディーの入力値をusecaseで使える形に変換する
 
 import (
 	"backend/model"
